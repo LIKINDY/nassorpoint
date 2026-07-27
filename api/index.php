4cd34ce23f74f84b0278a480aca3f73670ce1169
Likindy <likindyinfo@gmail.com>
2026-07-27 17:41:29 +0300

    Configure Laravel storage for Vercel Serverless environment

--- api/index.php
+++ api/index.php
@@ -1,4 +1,21 @@
 <?php
 
-// Forward Vercel requests to the normal Laravel public/index.php
+$appStorage = '/tmp/storage';
+
+$dirs = [
+    "$appStorage/framework/views",
+    "$appStorage/framework/cache/data",
+    "$appStorage/framework/sessions",
+    "$appStorage/logs"
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+putenv("VIEW_COMPILED_PATH=$appStorage/framework/views");
+putenv("LOG_CHANNEL=stderr");
+
 require __DIR__ . '/../public/index.php';
